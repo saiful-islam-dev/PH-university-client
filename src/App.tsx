b240aa295d6fad1a44f2bb68bd23c4d13a97873a
Saiful264 <saifulislamx264@gmail.com>
2024-01-16 23:38:14 +0600

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
