cc4c6a4868ab3bd2cc759ee714b5914f261aadc8
Saiful264 <saifulislamx264@gmail.com>
2024-02-11 01:06:20 +0600

    adding academicManagement code

---
+++ src/pages/admin/academicManagement/AcademicFaculty.tsx
@@ -0,0 +1,10 @@
+
+const AcademicFaculty = () => {
+    return (
+        <div>
+            <h1>AcademicFaculty</h1>
+        </div>
+    );
+};
+
+export default AcademicFaculty;
---
+++ src/pages/admin/academicManagement/CreateAcademicFaculty.tsx
@@ -0,0 +1,9 @@
+const CreateAcademicFaculty = () => {
+  return (
+    <div>
+      <h1>CreateAcademicFaculty</h1>
+    </div>
+  );
+};
+
+export default CreateAcademicFaculty;
